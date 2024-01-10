44a2c96efd281564a21738506cd9f3e3f03b8550
hunghx <baby10051999@gmail.com>
2024-01-10 08:48:19 +0700

    session1_baitap1

---
+++ Session1/src/baitap1/Main.java
@@ -0,0 +1,9 @@
+package baitap1;
+
+public class Main {
+    public static void main(String[] args) {
+        // nhập vào số a
+        // nhập va số b
+        // tính tổng và in ra màn hình
+    }
+}
